196b9a2f4425eedb0b3af0ede23fee4c4e271bc0
Stanley Filipe <stanley92filipe@gmail.com>
2022-06-05 17:37:45 -0300

    feat: save stripe event data in faunadb

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -47,6 +48,14 @@ export default async function handler(request: NextApiRequest, response: NextApi
       try {
         switch (type) {
           case 'checkout.session.completed':
+            
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+
             break
           default:
             throw new Error('Unhadled event.')
